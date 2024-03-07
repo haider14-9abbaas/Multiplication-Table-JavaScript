056faa1af3e9564aeec0da29c464fa90a97479cf
haider14-9abbaas <haiderzaidi290@gmail.com>
2024-03-07 19:58:03 +0500

    multiplication Table

---
+++ app.js
@@ -0,0 +1,9 @@
+document.write("JavaScript Connected")
+// Multiplication table from 2 to 10 using alert
+
+let table = "Multiplication Table of 2:\n";
+for (let i = 1; i <= 10; i++) {
+    table += "2 * " + i + " = " + (2 * i) + "\n";
+}
+alert(table);
+
